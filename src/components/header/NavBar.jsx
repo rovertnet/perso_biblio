72ccd06b8e9d8aee92c56855a6581da219a9a3e1
Robert Matundu <matundukabamba@gmail.com>
2025-01-25 23:38:47 +0100

    last commit

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -4,7 +4,12 @@ export default function NavBar() {
   return (
     <>
       <div className="fixed left-o right-0 top-0">
-        
+        <div className="flex justify-between items-center px-14 py-5">
+          <h1 className="font-bold text-xl text-slate-900">LOGO</h1>
+          <div className="flex justify-center items-center border-[1px] border-slate-900 space-x-5 px-3 py-2">
+            
+          </div>
+        </div>
       </div>
     </>
   )
