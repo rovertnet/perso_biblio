f5c61fdc84cf41683f4aa2ab40a48266b836b55f
Robert Matundu <matundukabamba@gmail.com>
2025-01-25 23:44:25 +0100

    last commit

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -7,10 +7,18 @@ export default function NavBar() {
         <div className="flex justify-between items-center px-14 py-5">
           <h1 className="font-bold text-xl text-slate-900">LOGO</h1>
           <div className="flex justify-center items-center border-[1px] border-slate-900 space-x-5 px-3 py-2">
-            <a href="#" className="font-bold text-xl text-slate-900"></a>
+            <a href="#" className="font-bold text-xl text-slate-900">
+              Accueil
+            </a>
+            <a href="#" className="font-bold text-xl text-slate-900">
+              Shop
+            </a>
+            <a href="#" className="font-bold text-xl text-slate-900">
+              Top books
+            </a>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
