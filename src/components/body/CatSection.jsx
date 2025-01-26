6f925bb76ec758148393468fb17ecd9e8a2a5826
Robert Matundu <matundukabamba@gmail.com>
2025-01-26 17:52:12 +0100

    last commit

--- src/components/body/CatSection.jsx
+++ src/components/body/CatSection.jsx
@@ -4,14 +4,55 @@ export default function CatSection() {
   return (
     <>
       <div className="block px-14 py-20">
-        <h2 className="font-bold text-lg text-slate-900 py-10">Toutes les catégories</h2>
+        <h2 className="font-bold text-lg text-slate-900 py-10">
+          Toutes les catégories
+        </h2>
         <div className="grid grid-col-4 gap-10">
           <div className="bg-white rounded-md px-5 py-3 ">
-            <img src={catimage} alt={catimage} className="rounded-full h-16 w-16" />
-            <span className="text-sm font-normal py-2 text-center text-slate-800">Science</span>
+            <img
+              src={catimage}
+              alt={catimage}
+              className="rounded-full h-16 w-16"
+            />
+            <span className="text-sm font-normal py-2 text-center text-slate-800">
+              Science
+            </span>
+          </div>
+
+          <div className="bg-white rounded-md px-5 py-3 ">
+            <img
+              src={catimage}
+              alt={catimage}
+              className="rounded-full h-16 w-16"
+            />
+            <span className="text-sm font-normal py-2 text-center text-slate-800">
+              Science
+            </span>
+          </div>
+
+          <div className="bg-white rounded-md px-5 py-3 ">
+            <img
+              src={catimage}
+              alt={catimage}
+              className="rounded-full h-16 w-16"
+            />
+            <span className="text-sm font-normal py-2 text-center text-slate-800">
+              Science
+            </span>
+          </div>
+
+          <div className="bg-white rounded-md px-5 py-3 ">
+            <img
+              src={catimage}
+              alt={catimage}
+              className="rounded-full h-16 w-16"
+            />
+            <span className="text-sm font-normal py-2 text-center text-slate-800">
+              Science
+            </span>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
