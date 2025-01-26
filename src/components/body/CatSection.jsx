a66da3ac1a562d6a4048a9f40079c9e8ff395ad6
Robert Matundu <matundukabamba@gmail.com>
2025-01-26 17:58:08 +0100

    last commit

--- src/components/body/CatSection.jsx
+++ src/components/body/CatSection.jsx
@@ -3,12 +3,12 @@ import catimage from "../../assets/image/RP1.jpeg"
 export default function CatSection() {
   return (
     <>
-      <div className="block px-14 py-20">
+      <div className="block px-14 py-6">
         <h2 className="font-bold text-lg text-slate-900 py-10">
           Toutes les catégories
         </h2>
-        <div className="grid grid-col-4 gap-10">
-          <div className="bg-white rounded-md px-5 py-3 ">
+        <div className="grid grid-cols-4 gap-10">
+          <div className="bg-white rounded-md px-5 py-3 shadow-md">
             <img
               src={catimage}
               alt={catimage}
