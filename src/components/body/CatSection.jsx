a0a78bf4da20c3b271a7ccd7ad84ac9911505387
Robert Matundu <matundukabamba@gmail.com>
2025-01-26 17:49:09 +0100

    last commit

--- src/components/body/CatSection.jsx
+++ src/components/body/CatSection.jsx
@@ -4,7 +4,7 @@ export default function CatSection() {
   return (
     <>
       <div className="block px-14 py-20">
-        <h2 className=""></h2>
+        <h2 className="font-bold text-"></h2>
         <div className="grid grid-col-4 gap-10">
           <div className="bg-white rounded-md px-5 py-3 ">
             <img src={catimage} alt={catimage} className="rounded-full h-16 w-16" />
