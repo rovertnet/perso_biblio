2350cf277c445dffe2d9ca736370a7d1dd3da52c
Robert Matundu <matundukabamba@gmail.com>
2025-01-26 17:18:22 +0100

    last commit

--- src/components/body/CatSection.jsx
+++ src/components/body/CatSection.jsx
@@ -4,7 +4,9 @@ export default function CatSection() {
   return (
     <>
       <div className="block px-14 py-20">
-        
+        <div className="grid grid-col-4 gap-10">
+          
+        </div>
       </div>
     </>
   )
