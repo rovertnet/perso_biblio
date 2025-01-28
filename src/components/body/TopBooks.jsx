47fcea6855bfb11cf828e15c5555e09a85d73a94
Robert Matundu <matundukabamba@gmail.com>
2025-01-28 23:23:55 +0100

    another

--- src/components/body/TopBooks.jsx
+++ src/components/body/TopBooks.jsx
@@ -8,7 +8,46 @@ export default function TopBooks() {
         <hr className="text-slate-300 " />
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 py-10">
-          <div className="bg-slate- px-5 py-5 shadow-md rounded-md">
+          <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
+            <img src={imgproduit} alt={imgproduit} className="rounded-md" />
+            <div className="pt-6 flex flex-col">
+              <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
+              <span className="font-normal text-sm text-slate-400">
+                Auteur du livre
+              </span>
+              <span className="font-normal text-sm text-slate-400">
+                Edition : année
+              </span>
+            </div>
+          </div>
+
+          <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
+            <img src={imgproduit} alt={imgproduit} className="rounded-md" />
+            <div className="pt-6 flex flex-col">
+              <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
+              <span className="font-normal text-sm text-slate-400">
+                Auteur du livre
+              </span>
+              <span className="font-normal text-sm text-slate-400">
+                Edition : année
+              </span>
+            </div>
+          </div>
+
+          <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
+            <img src={imgproduit} alt={imgproduit} className="rounded-md" />
+            <div className="pt-6 flex flex-col">
+              <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
+              <span className="font-normal text-sm text-slate-400">
+                Auteur du livre
+              </span>
+              <span className="font-normal text-sm text-slate-400">
+                Edition : année
+              </span>
+            </div>
+          </div>
+
+          <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
             <img src={imgproduit} alt={imgproduit} className="rounded-md" />
             <div className="pt-6 flex flex-col">
               <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
