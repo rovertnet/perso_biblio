f64aa9aecad35f942da968c1e1f0b136898555a6
Robert Matundu <matundukabamba@gmail.com>
2025-01-28 21:05:15 +0100

    another

--- src/components/body/TopBooks.jsx
+++ src/components/body/TopBooks.jsx
@@ -2,6 +2,15 @@
 
 export default function TopBooks() {
   return (
-    <div>TopBooks</div>
-  )
+    <>
+      <div className="block px-14 py-6 bg-[#f7f4f4]">
+        <h2 className="font-bold text-lg text-slate-900 py-5">
+          Toutes les catégories
+        </h2>
+        <hr className="text-slate-300 " />
+        
+        
+      </div>
+    </>
+  );
 }
