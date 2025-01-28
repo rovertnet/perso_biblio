6b5fd7ed033f0e067625fc43d0161dee8c4d410b
Robert Matundu <matundukabamba@gmail.com>
2025-01-28 20:47:08 +0100

    another

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,11 +1,11 @@
-
+import logo from "../../assets/image/mon_logo.png"
 
 export default function NavBar() {
   return (
     <>
       <div className="bg-white fixed top-0 right-0 left-0 shadow-md">
         <div className="flex justify-between items-center px-14 py-5">
-          <h1 className="font-bold text-xl text-slate-900">LOGO</h1>
+          <img src={logo} alt={logo} className="w-14 h-14" />
 
           <div className="">
             <div className="flex justify-center items-center border-[1px] border-slate-900 bg-slate-900 rounded-2xl space-x-5 px-3 py-2">
@@ -29,7 +29,7 @@ export default function NavBar() {
 
           <div className="flex space-x-3">
             <h1 className="font-bold text-xl text-slate-900">LOGO</h1>
-            <button className="font-bold text-lg md:text-lg py-2 md:py-3 px-3 md:px-4 bg-slate-900">Connexion</button>
+            <button className="font-bold text-lg md:text-lg py-2 md:py-2 px-3 md:px-3 text-slate-200 rounded-xl bg-slate-900">Connexion</button>
           </div>
         </div>
       </div>
