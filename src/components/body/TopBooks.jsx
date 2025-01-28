c0b43390b4d74d645f39aadd2e51cf995fa64ca4
Robert Matundu <matundukabamba@gmail.com>
2025-01-28 22:37:27 +0100

    another

--- src/components/body/TopBooks.jsx
+++ src/components/body/TopBooks.jsx
@@ -4,14 +4,12 @@ export default function TopBooks() {
   return (
     <>
       <div className="block px-14 py-6 bg-[rgb(247, 244, 244)]">
-        <h2 className="font-bold text-lg text-slate-900 py-5">
-          Top Livres
-        </h2>
+        <h2 className="font-bold text-lg text-slate-900 py-5">Top Livres</h2>
         <hr className="text-slate-300 " />
-        
+
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
           <div className="bg-white px-5 py-3">
-            <div className=""></div>
+            <div className="bg-[#eeecec]"></div>
           </div>
         </div>
       </div>
