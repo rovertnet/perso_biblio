a98d5e503b3fb6ee25558afcbeabc9caf2541a5a
Robert Matundu <matundukabamba@gmail.com>
2025-01-28 23:21:14 +0100

    another

--- src/components/body/TopBooks.jsx
+++ src/components/body/TopBooks.jsx
@@ -8,7 +8,7 @@ export default function TopBooks() {
         <hr className="text-slate-300 " />
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 py-10">
-          <div className="bg-white px-5 py-5 shadow-md rounded-md">
+          <div className="bg-slate- px-5 py-5 shadow-md rounded-md">
             <img src={imgproduit} alt={imgproduit} className="rounded-md" />
             <div className="pt-6 flex flex-col">
               <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
