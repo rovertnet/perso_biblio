7b8e56cd19aedc5145a8ce178d8b4030cd3e68df
Robert Matundu <matundukabamba@gmail.com>
2025-01-28 22:56:21 +0100

    another

--- src/components/body/TopBooks.jsx
+++ src/components/body/TopBooks.jsx
@@ -1,4 +1,4 @@
-
+import imgproduit from "../../assets/image/P2.jpg"
 
 export default function TopBooks() {
   return (
@@ -7,9 +7,9 @@ export default function TopBooks() {
         <h2 className="font-bold text-lg text-slate-900 py-5">Top Livres</h2>
         <hr className="text-slate-300 " />
 
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10 py-10">
           <div className="bg-white px-5 py-5 shadow-md rounded-md">
-            <div className="bg-[#eeecec] px-5 py-14 rounded-md"></div>
+            <img src={imgproduit} alt={imgproduit} className="" />
             <div className="py-6 px-5 flex"></div>
           </div>
         </div>
