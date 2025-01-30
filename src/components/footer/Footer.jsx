d760d2b3180a9f92261530f4b23a81c2e0754894
Robert Matundu <matundukabamba@gmail.com>
2025-01-30 16:51:11 +0100

    another commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,19 +8,19 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 export default function Footer() {
   return (
     <>
-      <div className="w-full py-20 bg-[#222020] px-14">
+      <div className="w-full py-12 bg-[#222020] px-14">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-14">
           <div className="flex flex-col gap-3">
-            <img src={logo} alt="logo" className="w-14 h-14" />
+            <img src={logo} alt="logo" className="w-16 h-16" />
             <p className="font-semibold text-lg text-slate-200 max-w-lg">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
               excepturi amet rerum labor
             </p>
             <div className="flex space-x-3">
-              <FaFacebook />
-              <FaInstagramSquare />
-              <FaLinkedin />
-              <FaSquareXTwitter />
+              <FaFacebook className="font-bold text-4xl text-blue-800" />
+              <FaInstagramSquare className="font-bold text-4xl text-red-400" />
+              <FaLinkedin className="font-bold text-4xl text-blue-500" />
+              <FaSquareXTwitter className="font-bold text-4xl text-black" />
             </div>
           </div>
 
