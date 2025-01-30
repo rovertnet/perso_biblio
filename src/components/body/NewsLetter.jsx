51a3d25b4cc0ca12d0be90c4b2e7e4cdf814c893
Robert Matundu <matundukabamba@gmail.com>
2025-01-30 13:48:20 +0100

    another commit

--- src/components/body/NewsLetter.jsx
+++ src/components/body/NewsLetter.jsx
@@ -5,7 +5,7 @@ export default function NewsLetter() {
       <div className="w-full bg-blue-300 py-28 px-14">
         <p className=""></p>
         <form action="#" className="">
-          <input type="mail" className="" />
+          <input type="mail" className="w-full px-5 py-3 text-" />
         </form>
       </div>
     </>
