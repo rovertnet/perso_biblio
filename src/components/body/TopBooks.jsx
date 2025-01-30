08b45798d3dc3f2e1e4c8a79f37fabc243513b80
Robert Matundu <matundukabamba@gmail.com>
2025-01-30 18:08:28 +0100

    another commit*

--- src/components/body/TopBooks.jsx
+++ src/components/body/TopBooks.jsx
@@ -12,6 +12,23 @@ export default function TopBooks() {
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 py-10">
           <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
+            <img
+              src={imgproduit}
+              alt={imgproduit}
+              className="rounded-md object-cover"
+            />
+            <div className="pt-6 flex flex-col">
+              <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
+              <span className="font-normal text-sm text-slate-400">
+                Auteur du livre
+              </span>
+              <span className="font-normal text-sm text-slate-400">
+                Edition : année
+              </span>
+            </div>
+          </div>
+
+          <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md object-cover">
             <img src={imgproduit} alt={imgproduit} className="rounded-md" />
             <div className="pt-6 flex flex-col">
               <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
@@ -25,7 +42,11 @@ export default function TopBooks() {
           </div>
 
           <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
-            <img src={imgproduit} alt={imgproduit} className="rounded-md" />
+            <img
+              src={imgproduit}
+              alt={imgproduit}
+              className="rounded-md object-cover"
+            />
             <div className="pt-6 flex flex-col">
               <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
               <span className="font-normal text-sm text-slate-400">
@@ -38,20 +59,11 @@ export default function TopBooks() {
           </div>
 
           <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
-            <img src={imgproduit} alt={imgproduit} className="rounded-md" />
-            <div className="pt-6 flex flex-col">
-              <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
-              <span className="font-normal text-sm text-slate-400">
-                Auteur du livre
-              </span>
-              <span className="font-normal text-sm text-slate-400">
-                Edition : année
-              </span>
-            </div>
-          </div>
-
-          <div className="bg-slate-100 px-5 py-5 shadow-md rounded-md">
-            <img src={imgproduit} alt={imgproduit} className="rounded-md" />
+            <img
+              src={imgproduit}
+              alt={imgproduit}
+              className="rounded-md object-cover"
+            />
             <div className="pt-6 flex flex-col">
               <h3 className="font-semibold text-lg text-slate-800">Titre</h3>
               <span className="font-normal text-sm text-slate-400">
