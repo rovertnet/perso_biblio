0b0efbf78c13a2c96d8db43e4b8ef491a0f13867
Robert Matundu <matundukabamba@gmail.com>
2025-01-30 15:44:20 +0100

    another commit

--- src/components/body/NewsLetter.jsx
+++ src/components/body/NewsLetter.jsx
@@ -5,18 +5,20 @@ export default function NewsLetter() {
       <div className="w-full bg-[#0c296d] py-16 px-14">
         <div className="flex flex-col md:flex-row gap-16">
           <p className="font-bold text-2xl text-justufy text-slate-200 max-w-md">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Quibusdam excepturi amet rerum labor
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            excepturi amet rerum labor
           </p>
           <form action="#" className="">
+            <div className="py-5">
               <input
                 type="mail"
                 className="w-full px-5 py-3 text-slate-900 outline-0 focus:outline-0 bg-white border-[1px] border-slate-700 rounded-lg"
                 placeholder="exemple@gmail.com"
               />
-              <button type="button" className="">
-                Button
+              <button type="button" className="py-3 px-5 font-bold text-white text-lg text-center">
+                Envoyer
               </button>
+            </div>
           </form>
         </div>
       </div>
