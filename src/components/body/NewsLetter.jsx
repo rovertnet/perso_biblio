0cd82f86892ef3d711623635bf970b960a3ec5a6
Robert Matundu <matundukabamba@gmail.com>
2025-01-30 13:42:12 +0100

    another commit

--- src/components/body/NewsLetter.jsx
+++ src/components/body/NewsLetter.jsx
@@ -2,7 +2,9 @@
 export default function NewsLetter() {
   return (
     <>
-      <div className="w-full bg-blue-"></div>
+      <div className="w-full bg-blue-300 py-28 px-14">
+        
+      </div>
     </>
   )
 }
