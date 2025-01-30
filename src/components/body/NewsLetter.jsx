86667731f10dc33c0474e6ee097dd6c403fd0052
Robert Matundu <matundukabamba@gmail.com>
2025-01-30 14:07:21 +0100

    another commit

--- src/components/body/NewsLetter.jsx
+++ src/components/body/NewsLetter.jsx
@@ -3,16 +3,18 @@ export default function NewsLetter() {
   return (
     <>
       <div className="w-full bg-[#0c296d] py-28 px-14">
-        <p className=""></p>
-        <form action="#" className="">
-          <div className="flex space-x-3">
-            <input
-              type="mail"
-              className="w-full px-5 py-3 text-slate-900 outline-0 focus:outline-0 bg-white border-[1px] border-slate-700 rounded-lg"
-              placeholder="exemple@gmail.com"
-            />
-          </div>
-        </form>
+        <div className="flex flex-col md:flex-row gap-16">
+          <p className="font-bold text-2xl text-justufy text-slate-200"></p>
+          <form action="#" className="">
+            <div className="flex space-x-3">
+              <input
+                type="mail"
+                className="w-full px-5 py-3 text-slate-900 outline-0 focus:outline-0 bg-white border-[1px] border-slate-700 rounded-lg"
+                placeholder="exemple@gmail.com"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
