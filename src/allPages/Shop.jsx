7e581492faee01fcef0e4b2249bcb77fbe554ac2
Robert Matundu <matundukabamba@gmail.com>
2025-02-01 00:20:02 +0100

    another commit*

--- src/allPages/Shop.jsx
+++ src/allPages/Shop.jsx
@@ -1,10 +1,14 @@
 import Shopbanner from "../components/body/shop/Shopbanner";
+import TopBooks from "../components/body/TopBooks"
+import Populaires from "../components/body/Populaires";
 
 
 export default function Shop() {
   return (
     <>
       <Shopbanner />
+      <TopBooks />
+      <Populaires />
     </>
   );
 }
