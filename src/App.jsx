567b8d97a6c1fa7816a6d689bb566986a518e669
Robert Matundu <matundukabamba@gmail.com>
2025-01-31 14:00:22 +0100

    another commit*

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import HomePage from "./allPages/HomePage";
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/header/NavBar"
 import Shop from "./allPages/Shop";
+import TopBooks from "./allPages/Topbooks";
+import About from "./allPages/About";
+import Contact from "./allPages/Contact";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const MyContext = createContext();
@@ -23,6 +26,9 @@ function App() {
           <Routes>
             <Route path="/" exact={true} element={<HomePage />} />
             <Route path="/shop" exact={true} element={<Shop />} />
+            <Route path="/topbooks" exact={true} element={<TopBooks />} />
+            <Route path="/about" exact={true} element={<About />} />
+            <Route path="/contact" exact={true} element={<Contact />} />
           </Routes>
         </MyContext.Provider>
       </BrowserRouter>
