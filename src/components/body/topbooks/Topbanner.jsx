cd31b6ca271cc113761c643c72c3761404491761
Robert Matundu <matundukabamba@gmail.com>
2025-02-01 00:37:44 +0100

    another commit*

--- src/components/body/topbooks/Topbanner.jsx
+++ src/components/body/topbooks/Topbanner.jsx
@@ -5,7 +5,7 @@ export default function Topbanner() {
     <>
       <div className="block h-full w-full py-32 px-32 hero">
         <h1 className="font-extrabold text-center text-5xl py-14 text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-blue-400 via-30% to-orange-500 to-90% ... uppercase">
-          NOS meilleurs livre
+          NOS meilleurs livres
         </h1>
 
         <div className="flex justify-center items-center">
