a8cf73b06fbc32ceac8a01147eaf6d5e103319ce
Robert Matundu <matundukabamba@gmail.com>
2025-01-31 13:22:28 +0100

    another commit*

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import HomePage from "./allPages/HomePage";
 import Footer from "./components/footer/Footer";
 import NavBar from "./components/header/NavBar"
 
+const MyContext = createContext();
+
 
 function App() {
- 
+  
+
 
   return (
     <>
       <NavBar />
       <HomePage />
+
+      
+
       <Footer />
     </>
   );
