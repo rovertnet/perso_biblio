10f9f6bd72551b084003d5681d3c57ff216ff48e
Robert Matundu <matundukabamba@gmail.com>
2025-02-02 22:28:51 +0100

    another commit*

--- src/components/body/about/AboutSection.jsx
+++ src/components/body/about/AboutSection.jsx
@@ -3,7 +3,7 @@ export default function AboutSection() {
   return (
     <>
       <div className="block px-14 py-6 bg-[#f7f4f4]">
-        
+        <div className=""></div>
       </div>
     </>
   );
