32587894756342bc8e6b9a7e0e0a5df40811fb08
Robert Matundu <matundukabamba@gmail.com>
2025-02-02 15:10:09 +0100

    another commit*

--- src/components/body/HeroSection.jsx
+++ src/components/body/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <>
       <div className="block h-full w-full py-32 px-32 hero">
-        <h1 className="font-extrabold text-center md:text-5xl text-3xl py-14 text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-blue-400 via-30% to-orange-500 to-90% ... uppercase">
+        <h1 className="font-extrabold text-center md:text-5xl text-2xl py-14 md: text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-blue-400 via-30% to-orange-500 to-90% ... uppercase">
           Bienvenu (e) <br /> à la grande biblothèque <br /> de la république
           démocratique du congo
         </h1>
