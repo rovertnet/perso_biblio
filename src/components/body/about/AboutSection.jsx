75c80ff4f3950e3690e8b259ad884f6c138015fe
Robert Matundu <matundukabamba@gmail.com>
2025-02-02 23:09:39 +0100

    another commit*

--- src/components/body/about/AboutSection.jsx
+++ src/components/body/about/AboutSection.jsx
@@ -28,29 +28,15 @@ export default function AboutSection() {
               quisquam voluptas eveniet, repudiandae reiciendis ab sapiente
               earum itaque cumque aliquid odit. Consequuntur, quia.
             </span>
-            <button className="font-bold bg-blue-950 text-slate-100 px-3 py-2 rounded-md text-start">
-              Contact-nous
-            </button>
+            <div className="flex justify-start items-start">
+              <button className="font-bold bg-blue-950 text-slate-100 px-3 py-2 rounded-md text-start">
+                Contact-nous
+              </button>
+            </div>
           </div>
 
           <div className="flex flex-col gap-5">
-            <img
-              src={aboutimg}
-              alt="about image"
-              className="rounded-md object-cover w-full h-full md:w-full md:h-full"
-            />
-            <h2 className="font-extrabold text-2xl text-slate-900 text-start">
-              Une brève explication sur notre bibliothèque
-            </h2>
-            <span className="font-semibold text-lg text-slate-700">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Molestias, ducimus sit distinctio a est quibusdam assumenda
-              quisquam voluptas eveniet, repudiandae reiciendis ab sapiente
-              earum itaque cumque aliquid odit. Consequuntur, quia.
-            </span>
-            <button className="font-bold bg-blue-950 text-slate-100 px-3 py-2 rounded-md">
-              Contact-nous
-            </button>
+            <div className="bg-"></div>
           </div>
         </div>
       </div>
