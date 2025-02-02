6f2f14e4afc59448b48b91e0380f77fdedeae530
Robert Matundu <matundukabamba@gmail.com>
2025-02-02 14:55:06 +0100

    another commit*

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -67,18 +67,16 @@ export default function NavBar() {
           <div className=" md:hidden">
             <button
               onClick={toggleMenu}
-              className="font-bold cursor-pointer text-lg md:text-lg py-3 px-3 text-slate-100 rounded-full bg-[#d2defb]"
+              className="font-bold cursor-pointer text-lg md:text-lg py-4 px-4 text-slate-100 rounded-full bg-[#d2defb]"
             >
               {openMenu ? (
                 <IoMdClose
-                  size={22}
                   className="text-3xl font-bold text-[#0c296d]"
                   aria-hidden="true"
                 />
               ) : (
                 <HiMenuAlt4
-                  size={22}
-                  className="text-3xl font-bold text-[#0c296d]"
+                  className="text-3xl font-extrabold text-[#0c296d]"
                   aria-hidden="true"
                 />
               )}
@@ -89,7 +87,13 @@ export default function NavBar() {
         {/* menu mobile */}
 
         <div className="md:hidden">
-          <div className="flex flex-col justify-items-center items-center gap-3.5 space-y-1 px-11 pb-3 sm:px-3">
+          <div
+            className={`flex flex-col justify-items-center items-center gap-3.5 space-y-1 px-11 pb-3 sm:px-3 ${
+              openMenu
+                ? ""
+                : "hidden"
+            }`}
+          >
             {navigation.map((item) => (
               <NavLink
                 key={item.name}
