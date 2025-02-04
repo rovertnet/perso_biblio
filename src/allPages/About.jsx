edf3e30fc05997631f616c91d03dff991355fa69
Robert Matundu <matundukabamba@gmail.com>
2025-02-04 17:09:02 +0100

    another commit

--- src/allPages/About.jsx
+++ src/allPages/About.jsx
@@ -1,12 +1,13 @@
 import AboutBanner from "../components/body/about/AboutBanner";
 import AboutSection from "../components/body/about/AboutSection";
+import PartenairesView from "../components/body/PartenairesView";
 
 export default function About() {
   return (
     <>
       <AboutBanner />
       <AboutSection />
-      <Partenaire />
+      <PartenairesView />
     </>
   );
 }
