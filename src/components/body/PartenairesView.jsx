e7e056b9f902088d9b4d955c41f885498ff8d8ea
Robert Matundu <matundukabamba@gmail.com>
2025-02-04 15:43:51 +0100

    another commit

---
+++ src/components/body/PartenairesView.jsx
@@ -0,0 +1,10 @@
+import Partenaires from "./Partenaires";
+
+
+export default function PartenairesView() {
+  return (
+    <>
+      <Partenaires autoplay={true} pauseOnHover={true}></Partenaires>
+    </>
+  );
+}
