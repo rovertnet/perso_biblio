de2aa9314a036219236fa1320fb9f753b38ea840
Robert Matundu <matundukabamba@gmail.com>
2025-02-13 15:10:40 +0100

    another commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {port: 8080},
+  server: {port: 8088},
 });
