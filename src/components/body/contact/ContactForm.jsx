c4bf38692d5d9927263d49dd7c13d5e904ee49db
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 16:58:24 +0100

    another commit

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -3,8 +3,8 @@ import { FaPhoneAlt } from "react-icons/fa";
 export default function ContactForm() {
   return (
     <>
-      <div className="block px-8 md:px-14 py-6 bg-black/95 md:-mt-24 -mt-6 md:mx-14 mx-8 md:mb-14 mb-6">
-        <div className="flex justify-center items-center md:py-10 py-9">
+      <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-14 mx-8 md:mb-14 mb-6">
+        <div className="flex justify-between items-center md:py-10 py-9">
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
