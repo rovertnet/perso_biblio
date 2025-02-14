def2e3c7821bb2916d4cfe0e5beb8931996b879a
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 16:59:33 +0100

    another commit

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -4,7 +4,7 @@ export default function ContactForm() {
   return (
     <>
       <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-14 mx-8 md:mb-14 mb-6">
-        <div className="flex justify-between items-center md:py-10 py-9">
+        <div className="flex justify-center items-center gap-14 md:py-10 py-9">
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
