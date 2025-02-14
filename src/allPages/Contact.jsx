2dae56a7352891d1a04fcca9412289fbaaf6414b
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 15:50:58 +0100

    another commit

--- src/allPages/Contact.jsx
+++ src/allPages/Contact.jsx
@@ -1,10 +1,12 @@
 import ContactBanner from "../components/body/contact/ContactBanner";
+import ContactForm from "../components/body/contact/ContactForm";
 
 
 export default function Contact() {
   return (
     <>
       <ContactBanner />
+      <ContactForm />
     </>
-  )
+  );
 }
