e804ec3726e51f014c17b3d40372709b6b151a8b
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 17:46:41 +0100

    another commit

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -10,11 +10,11 @@ export default function ContactForm() {
       <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-28 mx-8 md:mb-14 mb-6 rounded-md">
         <div className="flex justify-center items-center md:gap-32 md:py-10 py-9">
           <div className="flex flex-col gap-2">
-            <FaPhoneAlt className="font-bold text-blue-100 text-5xl" />
+            <FaPhoneAlt className="font-bold text-blue-100 text-4xl" />
           </div>
-          <div className="flex flex-col gap-2"><SiGooglemaps className="font-bold text-blue-100 text-5xl" /></div>
-          <div className="flex flex-col gap-2"><BiWorld className="font-bold text-blue-100 text-5xl" /></div>
-          <div className="flex flex-col gap-2"><IoMail className="font-bold text-blue-100 text-5xl" /></div>
+          <div className="flex flex-col gap-2"><SiGooglemaps className="font-bold text-blue-100 text-4xl" /></div>
+          <div className="flex flex-col gap-2"><BiWorld className="font-bold text-blue-100 text-4xl" /></div>
+          <div className="flex flex-col gap-2"><IoMail className="font-bold text-blue-100 text-4xl" /></div>
         </div>
       </div>
     </>
