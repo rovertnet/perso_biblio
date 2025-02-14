74c2d351f39176c367b8925314313f64255011a7
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 17:11:11 +0100

    another commit

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -1,4 +1,6 @@
 import { FaPhoneAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+
 
 export default function ContactForm() {
   return (
@@ -6,10 +8,8 @@ export default function ContactForm() {
       <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-14 mx-8 md:mb-14 mb-6">
         <div className="flex justify-center items-center md:gap-36 md:py-10 py-9">
           <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
-          <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
-          <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
-          <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
-          <FaPhoneAlt className="font-bold text-blue-100 text-3xl" />
+          <IoMdMail className="font-bold text-blue-100 text-3xl" />
+          <IoMdMail className="font-bold text-blue-100 text-3xl" />
         </div>
       </div>
     </>
