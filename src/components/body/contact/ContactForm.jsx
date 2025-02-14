1135f66f81b21544c35acc7962b29a9cc7e89148
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 16:15:40 +0100

    another commit

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -3,8 +3,8 @@
 export default function ContactForm() {
   return (
     <>
-      <div className="block px-8 md:-px-14 -py-6 ">
-
+      <div className="block px-8 md:px-14 py-6 bg-white/10 md:-mt-14 -mt-6">
+          <div className="flex justify-center items-center md:"></div>
       </div>
     </>
   );
