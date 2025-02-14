3dd6569f607f16378dfd93834ebc300366d0fa0a
Robert Matundu <matundukabamba@gmail.com>
2025-02-14 18:06:54 +0100

    another commit

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -9,28 +9,28 @@ export default function ContactForm() {
     <>
       <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-28 mx-8 md:mb-14 mb-6 rounded-md">
         <div className="flex justify-center items-center md:gap-32 md:py-10 py-9">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-3">
             <FaPhoneAlt className="font-bold text-blue-100 text-4xl" />
-            <h3 className="font-semibold text-lg text-slate-400 text-center">
-              +243 998724266 / 823444705
+            <h3 className="font-semibold text-lg text-slate-400 ">
+              +243 998724266
             </h3>
           </div>
-          <div className="flex flex-col gap-2">
-            <SiGooglemaps className="font-bold text-blue-100 text-4xl" />
-            <h3 className="font-semibold text-lg text-slate-400 text-center">
-              0023, mudjiba, Gombe/boulvard du 30 juin
+          <div className="flex flex-col gap-3">
+            <SiGooglemaps className="font-bold text-blue-100 text-4xl flex justify-center items-center" />
+            <h3 className="font-semibold text-lg text-slate-400 ">
+              boulvard du 30 juin
             </h3>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-3">
             <BiWorld className="font-bold text-blue-100 text-4xl" />
-            <h3 className="font-semibold text-lg text-slate-400 text-center"> 
-              République Démocratique du Congo (RDC)
+            <h3 className="font-semibold text-lg text-slate-400 "> 
+              RD Congo
             </h3>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-3">
             <IoMail className="font-bold text-blue-100 text-4xl" />
-            <h3 className="font-semibold text-lg text-slate-400 text-center">
-              matundukabamba@gmail.com / rovertnet.roberto@gmail.com
+            <h3 className="font-semibold text-lg text-slate-400 ">
+              rovertnet.roberto@gmail.com
             </h3>
           </div>
         </div>
