edbc546f7765f1afd1ca1824cfbae243598c8040
Robert Matundu <matundukabamba@gmail.com>
2025-02-17 11:12:36 +0100

    another

--- src/components/body/contact/ContactForm.jsx
+++ src/components/body/contact/ContactForm.jsx
@@ -7,8 +7,8 @@ import { IoMail } from "react-icons/io5";
 export default function ContactForm() {
   return (
     <>
-      <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-28 mx-8 md:mb-14 mb-6 rounded-md">
-        <div className="flex justify-center items-center md:gap-32 md:py-10 py-9">
+      <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-32 md:mx-28 mx-8 md:mb-14 mb-6 rounded-md">
+        <div className="flex flex-col md:flex-row justify-center items-center md:gap-32 md:py-10 py-9">
           <div className="flex flex-col gap-3">
             <FaPhoneAlt className="font-bold text-blue-100 text-4xl" />
             <h3 className="font-semibold text-lg text-slate-400 ">
