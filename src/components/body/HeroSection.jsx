0e68bb48ed09b5e54e7134d8cf5ebc8f3a59f780
Robert Matundu <matundukabamba@gmail.com>
2025-02-17 14:57:42 +0100

    another

--- src/components/body/HeroSection.jsx
+++ src/components/body/HeroSection.jsx
@@ -1,20 +1,15 @@
 export default function HeroSection() {
   return (
-    <>
-      <div className="block h-full w-full py-32 px-32 hero object-cover md:object-cover">
-        <h1 className="font-extrabold flex md:justify-center md:items-center md:text-center md:text-3xl md:max-w-6xl max-w-96 text-center text-base py-10  md:py-14 text-blue-200 uppercase">
-          Bienvenue à la grande bibliothèque de la République
-          Démocratique du Congo
-        </h1>
-
-        <div className="flex justify-center items-center">
-          <>
-            <button className="font-bold cursor-pointer hover:bg-black hover:border-0 text-lg md:text-xl text-white border-[1px] border-white rounded-xl py-2 md:py-4 px-2 md:px-3">
-              Contactez-nous
-            </button>
-          </>
-        </div>
+    <div className="block h-full w-full py-32 px-32 hero object-cover md:object-cover">
+      <h1 className="font-extrabold flex md:justify-center md:items-center md:text-center md:text-3xl md:max-w-6xl max-w-96 text-center text-base py-10 md:py-14 text-blue-200 uppercase">
+        Bienvenue à la grande bibliothèque de la République Démocratique du
+        Congo
+      </h1>
+      <div className="flex justify-center items-center">
+        <button className="font-bold cursor-pointer hover:bg-black hover:border-0 text-lg md:text-xl text-white border-[1px] border-white rounded-xl py-2 md:py-4 px-2 md:px-3">
+          Contactez-nous
+        </button>
       </div>
-    </>
+    </div>
   );
 }
