91dee540f4f3344f8d1de0507cd81b47e7c5a537
Robert Matundu <matundukabamba@gmail.com>
2025-02-17 10:27:19 +0100

    another

---
+++ src/components/body/contact/ContactForm.jsx
@@ -0,0 +1,47 @@
+import { FaPhoneAlt } from "react-icons/fa";
+import { SiGooglemaps } from "react-icons/si";
+import { BiWorld } from "react-icons/bi";
+import { IoMail } from "react-icons/io5";
+
+
+export default function ContactForm() {
+  return (
+    <>
+      <div className="block px-8 md:px-14 py-6 bg-black/85 md:-mt-24 -mt-6 md:mx-28 mx-8 md:mb-14 mb-6 rounded-md">
+        <div className="flex justify-center items-center md:gap-32 md:py-10 py-9">
+          <div className="flex flex-col gap-3">
+            <FaPhoneAlt className="font-bold text-blue-100 text-4xl" />
+            <h3 className="font-semibold text-lg text-slate-400 ">
+              +243 998724266
+            </h3>
+          </div>
+          <div className="flex flex-col gap-3">
+            <SiGooglemaps className="font-bold text-blue-100 text-4xl flex justify-center items-center" />
+            <h3 className="font-semibold text-lg text-slate-400 ">
+              boulvard du 30 juin
+            </h3>
+          </div>
+          <div className="flex flex-col gap-3">
+            <BiWorld className="font-bold text-blue-100 text-4xl" />
+            <h3 className="font-semibold text-lg text-slate-400 "> 
+              RD Congo
+            </h3>
+          </div>
+          <div className="flex flex-col gap-3">
+            <IoMail className="font-bold text-blue-100 text-4xl" />
+            <h3 className="font-semibold text-lg text-slate-400 ">
+              rovertnet.roberto@gmail.com
+            </h3>
+          </div>
+        </div>
+
+        {/* Formulaire de contact */}
+        <div className="pt-10">
+          <form action="#" className="w-full md:w-full">
+            <div className=""></div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
