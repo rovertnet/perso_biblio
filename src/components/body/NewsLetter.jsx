4b7b5a35a42f5dde24bb5d31ffc337160d8a14f6
Robert Matundu <matundukabamba@gmail.com>
2025-02-17 13:39:50 +0100

    another

--- src/components/body/NewsLetter.jsx
+++ src/components/body/NewsLetter.jsx
@@ -1,17 +1,16 @@
-
 export default function NewsLetter() {
   return (
     <>
       <div className="w-full bg-[#0c296d] py-14 px-8 md:px-14">
         <div className="flex flex-col md:flex-row gap-16">
-          <p className="font-bold text-4xl text-justufy text-slate-200 md:max-w-lg ">
+          <p className="font-bold text-4xl text-justify text-slate-200 md:max-w-lg ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
             excepturi amet rerum labor
           </p>
           <form action="#" className="w-full sm:w-full md:w-full lg:w-full">
             <div className="py-5 flex space-x-3">
               <input
-                type="mail"
+                type="email"
                 className="w-full px-5 py-3 text-slate-900 outline-0 focus:outline-0 sm:w-full md:w-full lg:w-full bg-white border-[1px] border-slate-700 rounded-lg"
                 placeholder="exemple@gmail.com"
               />
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -54,10 +54,16 @@ export default function NavBar() {
 
           <div className=" hidden sm:hidden md:block lg:block">
             <div className="flex space-x-3">
-              <button className="font-bold cursor-pointer text-lg md:text-lg py-2 md:py-2 px-3 md:px-3 text-[#d2defb] rounded-xl bg-[#0c296d] hover:bg-blue-00">
+              <button
+                aria-label="Sign up"
+                className="font-bold cursor-pointer text-lg md:text-lg py-2 md:py-2 px-3 md:px-3 text-[#d2defb] rounded-xl bg-[#0c296d] hover:bg-blue-00"
+              >
                 Sign up
               </button>
-              <button className="font-bold border-[1px] border-[#0c296d] cursor-pointer text-lg md:text-lg py-3 md:py-3 px-3 md:px-3 text-slate-100 rounded-full bg-[#d2defb]">
+              <button
+                aria-label="Cart"
+                className="font-bold border-[1px] border-[#0c296d] cursor-pointer text-lg md:text-lg py-3 md:py-3 px-3 md:px-3 text-slate-100 rounded-full bg-[#d2defb]"
+              >
                 <FaCartArrowDown className="text-xl font-bold text-[#0c296d]" />
               </button>
             </div>
@@ -68,6 +74,7 @@ export default function NavBar() {
             <button
               onClick={toggleMenu}
               className="font-bold cursor-pointer text-lg md:text-lg py-3 px-3 text-slate-100 rounded-full bg-[#d2defb] duration-1000 transition-all"
+              aria-label={openMenu ? "Close menu" : "Open menu"}
             >
               {openMenu ? (
                 <IoMdClose
