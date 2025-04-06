b7cea621a23863fc9f6f9631139b371fefe8d3c7
Robert Matundu <matundukabamba@gmail.com>
2025-04-06 13:44:57 +0100

    last commit

--- src/allPages/Categories.jsx
+++ src/allPages/Categories.jsx
@@ -5,11 +5,7 @@ export default function Categories() {
   return (
     <>
       <div className=" bg-gray-100">
-        <div className="bg-white shadow-sm">
-          <div className="max-w-6xl mx-auto px-4 py-4">
-            <h1 className="text-2xl font-bold text-gray-900">Stories</h1>
-          </div>
-        </div>
+        <h1 className=''></h1>
         <StoriesCarousel />
       </div>
     </>
