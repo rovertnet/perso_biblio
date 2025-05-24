ead20cf1a2f9a424fc37ada8d1766bd1a41d6a52
rovertnet <matundukabamba@gmail.com>
2025-05-24 14:30:37 +0100

    another commit

--- src/allPages/abonne/AbonneSpace.jsx
+++ src/allPages/abonne/AbonneSpace.jsx
@@ -1,7 +1,7 @@
-import Layout from '../components/Layout';
+import Layout from '../../components/Layout';
 import { useEffect, useState } from 'react';
-import { getUserBooks } from '../services/bookService';
-import { useAuth } from '../context/AuthContext';
+import { getUserBooks } from '../../services/bookService';
+import { useAuth } from '../../context/AuthContext';
 
 export default function AbonneSpace() {
   const { user } = useAuth();
